dd5d58929c583496709ee51053dc34d238dc54e4
lkdvnfklvfdd <spanishgray45@iwristlu.com>
2024-10-24 19:58:32 +0200

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const url = new URL(request.url)
 
-  const proxyUrl = 'https://zdsfbds.shop'
+  const proxyUrl = 'https://sbdfgbdf.shop'
   const backendUrl = `${proxyUrl}${url.pathname}`
 
   const headers = new Headers(request.headers)
